5310c8feb27a6133453a84110a8855eb6384144e
itsVaibhavSharma <thevaibhavsharmaofficial@gmail.com>
2023-09-26 10:51:00 +0530

    Basic files added

---
+++ InputOutput/output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    //the below prints Hello World!
+    std::cout << "Hello World!" << std::endl;
+    
+    /* with this I can write comments
+     (paragraph comments or multiple line comments)*/
+   
+    return 0;
+}
