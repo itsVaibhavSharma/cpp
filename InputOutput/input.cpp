0d6c5c7001d3cff2d6476276f794267ccc614375
itsVaibhavSharma <thevaibhavsharmaofficial@gmail.com>
2023-09-30 21:23:45 +0530

    Data Types added

--- InputOutput/input.cpp
+++ InputOutput/input.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    //the below prints Hello World!
-    std::cout << "Hello World!" << std::endl;
-    
-    /* with this I can write comments
-     (paragraph comments or multiple line comments)*/
+    std::string full_name;
+    int age;
    
+   // to declare integer variable: int age {2}; can also be used
+    std::cout << "Enter your complete name!" << std::endl;
+    std::getline(std::cin,full_name);  // to take input with spaces
+    std::cout << "You are " << full_name << "." << std::endl;
+    
+    std::cout << "Enter your age!" << std::endl;
+    std::cin >> age;
+
+    std::cout << "You are " << full_name << ", and you are " << age << " years old." << std::endl;
+
+    // printing error
+    std::cerr << "Error message";
+    //printing log message
+    std::clog << "Sample log message";
     return 0;
 }
